562aeacc577f17ce36f1613e6342ab6717586121
Iman Khorshidi <imankhorshidi72@gmail.com>
2022-09-14 23:32:26 +0430

    Add: Inter Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      Inter: ["Inter", "sans-serif"],
+    },
+    colors: {
+      primary: "#6acdee",
+      secondary: "#f6b319",
+      tertiary: "#c071ae",
+      accent: "#10103c",
+    },
   },
   plugins: [],
 };
